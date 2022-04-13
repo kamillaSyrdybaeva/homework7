e47832f476da1415dcc5f623e3c5a79cc30909d2
KamillaSyrdybaeva <syrdybaevakamilla@gmail.com>
2022-04-14 00:41:40 +0600

    ...

---
+++ src/com/company/Warrior.java
@@ -0,0 +1,7 @@
+package com.company;
+public class Warrior extends Hero {
+    @Override
+    public void applySuperAbility(String superAbilityType) {
+        System.out.println("Warrior used a weapon");
+    }
+}
